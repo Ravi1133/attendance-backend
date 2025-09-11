e113700341513f410a81b86159ffce545d9c0b5a
ravikant <ravidwivedi135581133@gmail.com>
2025-09-11 13:26:03 +0530

    Added changes

--- db/connection.js
+++ db/connection.js
@@ -5,7 +5,9 @@ dotenv.config({})
 let uri=process.env.DB_URL
 console.log("uri",uri)
 const connection=()=>{
-    mongoose.connect(uri).then((res)=>{
+    mongoose.connect(uri,{
+        autoIndex:true
+    }).then((res)=>{
         console.log("Connected")
     }).catch(err=>{
         console.log("err",err)
